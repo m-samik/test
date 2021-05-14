25ed70fe9525157e101d3b9d7404c19a841528f6
Muhammad Sami Khanday <61789670+m-samik@users.noreply.github.com>
2021-05-14 11:57:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<htmL>
+<head>
+<title>Terraform Web APP</title>
+</head>
+<body>
+<center><h1>I am Provisioned using Terraform </h1></center>
+<?php
+print `/usr/sbin/ifconfig`
+?>
+</body>
+</html>
